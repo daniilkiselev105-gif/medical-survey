144df7e77cb969c02bc27aa60902c0f8dcc5cf4e
daniilkiselev105-gif <daniilkiselev105@gmail.com>
2026-05-27 01:07:59 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,21 +8,13 @@ getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyB30FiMevIxKh-yi2VzclbpuXk89ZwhYdI",
-
-authDomain: "medical-survey-7a87e.firebaseapp.com",
-
-projectId: "medical-survey-7a87e","YOUR_PROJECT_ID",
-
-storageBucket: "medical-survey-7a87e.firebasestorage.app",
-
-messagingSenderId: "676904573806",
-
-appId: "1:676904573806:web:304d6cd534e499194aff7f",
-
-measurementId: "G-CWERWZC4D5"
-
+ apiKey: "AIzaSyB30FiMevIxKh-yi2VzclbpuXk89ZwhYdI",
+ authDomain: "medical-survey-7a87e.firebaseapp.com",
+ projectId: "medical-survey-7a87e",
+ storageBucket: "medical-survey-7a87e.firebasestorage.app",
+ messagingSenderId: "676904573806",
+ appId: "1:676904573806:web:304d6cd534e499194aff7f",
+ measurementId: "G-CWERWZC4D5"
 };
 
 const app = initializeApp(firebaseConfig);
